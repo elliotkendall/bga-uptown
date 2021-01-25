94eed81ac6e59dbbc1bb189e4839c4716570a03f
Elliot Kendall <elliotkendall@gmail.com>
2021-01-25 12:38:52 -0800

    Remove commented example to prevent warnings from automated checker

--- uptown.action.php
+++ uptown.action.php
@@ -40,27 +40,4 @@ class action_uptown extends APP_GameAction {
     $this->game->playTile($tile_id, $location);
     self::ajaxResponse();
   }
-    /*
-    
-    Example:
-  	
-    public function myAction()
-    {
-        self::setAjaxMode();     
-
-        // Retrieve arguments
-        // Note: these arguments correspond to what has been sent through the javascript "ajaxcall" method
-        $arg1 = self::getArg( "myArgument1", AT_posint, true );
-        $arg2 = self::getArg( "myArgument2", AT_posint, true );
-
-        // Then, call the appropriate method in your game logic, like "playCard" or "myAction"
-        $this->game->myAction( $arg1, $arg2 );
-
-        self::ajaxResponse( );
-    }
-    
-    */
-
-  }
-  
-
+}
